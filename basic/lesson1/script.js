a28445ca1dd8bcacc22b833f6f5729a07fa0b60d
VictorKmin <kminvictor@gmail.com>
2021-02-01 22:17:54 +0200

    Initial commit

---
+++ basic/lesson1/script.js
@@ -0,0 +1,117 @@
+// // alert('dasssdasasada');
+//
+// const name = 'Viktor';
+// let age = 25;
+// let car = true;
+// let money = false;
+//
+// // name = 'Dimas';
+// // age = 'Twenty five';
+//
+// // document.write('Hello my name is ' + name + ' and I am ' + age + ' years old');
+//
+// // document.write(2 + 2);
+// // document.write('<hr>');
+// // document.write(2 - 2);
+// // document.write('<hr>');
+// // document.write(2 * 2);
+// // document.write('<hr>');
+// // document.write(2 / 2);
+// // document.write('<hr>');
+// // document.write((2 + 2) * 2);
+// // document.write('<hr>');
+// // document.write(8 % 3);
+//
+// // console.log(true + 2);
+// // console.log(false + 5);
+// // console.log(age + '1');
+// // console.log(age - '1');
+// // console.log(age - 'Hello');
+// // console.log(age * '5');
+// // console.log(age / '5');
+//
+//
+// console.log(typeof age);
+// console.log(typeof true);
+// console.log(typeof 'Hello');
+// console.log(typeof NaN);
+//
+// let userName = prompt('What is your name?');
+// let isConfirmedAge = confirm('Are u 18+?');
+//
+// document.write('Hello ' + userName);
+//
+// console.log(isConfirmedAge);
+
+
+// let userAge = prompt('How old are u?');
+// let userName = prompt('ENTER NAME');
+//
+// // < > <= >= != == !== ===
+//
+// // console.log(3 > 3)
+// // console.log('31' != 31)
+// // console.log('3' !== 3)
+// // console.log('3' == 3);
+// // console.log('3' === 3);
+//
+// // if (userAge > 18) {
+// //   alert('WELCOME');
+// // } else {
+// //   if (userName === 'taras') {
+// //     alert('WELCOME');
+// //   } else {
+// //     alert('Too young!');
+// //   }
+// // }
+//
+// // if (userAge > 18) {
+// //   alert('WELCOME');
+// // } else if (userName === 'taras') {
+// //   alert('WELCOME');
+// // } else {
+// //   alert('Too young!');
+// // }
+//
+// // || -> OR
+// // && -> AND
+//
+// const isOldTaras = userAge > 18 || userName === 'taras';
+//
+// if (isOldTaras) {
+//   alert('WELCOME');
+// } else {
+//   alert('Too young!');
+// }
+
+
+// switch (userName) {
+//   case 'Viktor':
+//     console.log('Hello man!');
+//     break;
+//   case 'Ivan':
+//     console.log('Hi bro');
+//     break;
+//   case 'Olga':
+//     console.log('Hi girl');
+//     break;
+//   case 'Dimas':
+//     console.log('Lets get party');
+//     break;
+//   default:
+//     console.log('I dont know u');
+// }
+
+
+if (2 && 3 && 4 || 0) {
+  alert('OK')
+} else {
+  alert('NE OK');
+}
+
+// 0
+// ''
+// undefined
+// null
+// false
+// NaN
