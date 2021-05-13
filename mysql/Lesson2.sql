e138b617de6f61025f1b91de8bea20e6ce520da7
Victor Kmin <kminvictor@gmail.com>
2021-05-13 21:41:58 +0300

    Update Lesson2.sql

--- mysql/Lesson2.sql
+++ mysql/Lesson2.sql
@@ -1,45 +1,45 @@
-SELECT 
- c.model AS carModel,
- u.name AS userName 
-FROM cars c JOIN user u ON u.car_id = c.id;
+INSERT INTO cars(model) VALUES('Lacetti');
 
-
-SELECT * FROM ratings r
-   JOIN lesson l ON l.id = r.lesson_id
-   JOIN students s ON s.id = r.student_id
-   WHERE r.rating = 5;
-   
+INSERT INTO users(name, car_id) VALUES('Olga', 99);
 
 SELECT 
-s.name,
-s.age,
-l.date,
-l.label
- FROM ratings r
-   JOIN lesson l ON l.id = r.lesson_id
-   JOIN students s ON s.id = r.student_id
-   WHERE r.rating = 5;
-   
+	c.model AS carModel,
+    u.name AS userName
+FROM users u JOIN cars c ON u.car_id = c.id;
+
 
 SELECT 
- COUNT(r.rating) AS bestCount,
- s.name
-FROM ratings r  
+	s.name,
+    s.age,
+    l.label,
+    l.date
+FROM ratings r 
+JOIN students s ON r.student_id = s.id
+JOIN lesson l ON r.lesson_id = l.id
+WHERE rating = 5;
+
+
+SELECT 
+	COUNT(r.rating) AS ratingCount,
+    s.name,
+    s.id
+FROM ratings r
 JOIN students s ON r.student_id = s.id
 WHERE r.rating = 5
 GROUP BY r.student_id;
 
 
-SELECT CONCAT(name, '-', age) as fullName FROM students;
+SELECT CONCAT(name, '-', age) AS fullName, age, gender FROM students;
 
-SELECT DISTINCT(name) FROM students;
+SELECT DISTINCT(gender) FROM students;
 
-SELECT * FROM students WHERE id IN (SELECT student_id FROM ratings WHERE rating = 5);
+SELECT * FROM students WHERE id IN ( SELECT student_id FROM ratings WHERE rating = 5 );
 
 
 
-SELECT * FROM cars LEFT JOIN user ON user.car_id = cars.id;
 
-SELECT * FROM cars RIGHT JOIN user ON user.car_id = cars.id;
+SELECT * FROM users JOIN cars ON users.car_id = cars.id;
 
-SELECT * FROM cars JOIN user ON user.car_id = cars.id;
+SELECT * FROM users LEFT JOIN cars ON users.car_id = cars.id;
+
+SELECT * FROM users RIGHT JOIN cars ON users.car_id = cars.id;
